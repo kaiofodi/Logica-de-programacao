8e57b9b4c407d4c0e7b3ba0033d081f7186ea367
kaiox <kaio_fodi@estudante.sesisenai.org.br>
2026-02-25 21:41:09 -0300

    arquivos da aula 25 de fev 2025

---
+++ e os guri meu/e os guri.js
@@ -0,0 +1,12 @@
+/*
+//console.log("Hello, World!");
+//console.log("Hello, World!");
+//console.log("Hello, World!");
+*/
+
+console.log(1+1)             //soma
+console.log("1"+"1")         // concatenação 
+
+console.log("1"+1)
+console.log("1"-1)
+console.log("n"-1)
