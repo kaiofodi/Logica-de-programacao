ec92678d0af0bb22b026d937032cd6302371606e
KAIO ROBERTO FODI <kaio_fodi@estudante.sesisenai.org.br>
2026-05-20 20:51:54 -0300

    jogo-da-velha

--- e os guri meu/jogodavelha.js
+++ e os guri meu/jogodavelha.js
@@ -9,7 +9,7 @@ let guri = require('readline-sync');
 console.log('BEM-VINDOS AO JOGO DA VELHA');
 
 
-const jogoDaVelha = [
+const jogoDaVelha = [   // comeca-se com um tabuleiro 3x3 vazio.
     ["-",  "-",  "-"],
     ["-",  "-",  "-"],
     ["-",  "-",  "-"],
@@ -20,7 +20,8 @@ let j;
 
 console.table(jogoDaVelha);
 
-
+// utilizo um for e seguido de um while, para perguntar ao jogador qual posicao ele gostaria, com linha[i] e coluna[j]
+// e se caso a posicao ja tenha sido selecionada antes, tem o else fazendo com que repita a pergunta.
 for(let i = 1; jogoDaVelha <= 5; i++){
     } while(true){
         i = guri.questionInt(`Jogador ${jogador} - Digite a linha (0 a 2): `);
@@ -31,10 +32,12 @@ for(let i = 1; jogoDaVelha <= 5; i++){
       console.log("Posição já ocupada! Escolha outra.");
     }
   }
+// aqui digo que o jogo da velha nos valores que o jogador escolheu, armazenando na tentativa do jogador
 jogoDaVelha[i][j] = jogador
-    
+// so um .table para atualizar o tabuleiro    
 console.table(jogoDaVelha)
 
+//aqui so alterno entre os jogadores
 jogador = jogador === 'x' ? 'o' : 'x'
 
 for(let i = 1; jogoDaVelha <= 5; i++){
@@ -53,7 +56,7 @@ console.table(jogoDaVelha)
 
 jogador = jogador === 'x' ? 'o' : 'x'
 
-
+//====================== So repeticao do codigo ======================
 for(let i = 1; jogoDaVelha <= 5; i++){
     } while(true){
         i = guri.questionInt(`Jogador ${jogador} - Digite a linha (0 a 2): `);
