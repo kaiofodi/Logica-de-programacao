94c3da1b0cb66a4b2c254b63f2d1a4927baf7cd3
bubi <kaio_fodi@estudante.sesisenai.org.br>
2026-05-20 16:26:29 -0300

    desafio-aula-05
    desafio-aula-05

--- e os guri meu/desafios-dos-guri/desafio-aula-05.js
+++ e os guri meu/desafios-dos-guri/desafio-aula-05.js
@@ -28,7 +28,7 @@ let lerTeclado = require('readline-sync')
 // e) Exiba cada resultado com template literal identificando as pessoas pelo nome.
 
 // → Seu código aqui:
-
+/*
 const pessoaUm = {
     nome1: lerTeclado.question('Diga um nome: '),
     idade1: lerTeclado.questionInt('Diga a idade dessa pessoa: ')
@@ -55,7 +55,8 @@ let pessoaTresMaiordeIdade = pessoaTres.idade3 >= 18;
 
 console.log(`Primeira pessoa:
 Nome: ${pessoaUm.nome1}
-Idade: ${pessoaUm.idade1}
+Preco: ${dadosUm.preco1}
+Estoque: ${dadosUm.qtdEstoque}
 Segunda pessoa:
 Nome: ${pessoaDois.nome2}
 Idade: ${pessoaDois.idade2}
@@ -68,7 +69,7 @@ A terceira pessoa e maior de idade: ${pessoaTresMaiordeIdade}`);
 console.log(`A primeira pessoa e mais velha que a segunda: ${pessoa1maisVelhaQuePessoa2}
 A segunda pessoa e mais nova que a terceira: ${pessoa2maisNovaQuePessoa3}
 A terceira pessoa tem a mesma idade da terceira: ${pessoa1mesmaIdadePessoa3}`);
-
+*/
 
 
 console.log("_______________________________");
@@ -88,8 +89,29 @@ console.log("_______________________________");
 // e) Exiba cada resultado identificando os produtos pelo nome.
 
 // → Seu código aqui:
+/*
+const produtoUm = {
+    nome1: lerTeclado.question('Diga um produto: '),
+    valor1: lerTeclado.questionFloat('Diga o valor desse produto: ')
+};
+const produtoDois = {
+    nome2: lerTeclado.question('Diga um produto: '),
+    valor2: lerTeclado.questionFloat('Diga o valor desse produto: ')
+};
+let diferenca = Math.abs(produtoUm.valor1 - produtoDois.valor2) 
+let produtos = [produtoUm, produtoDois]
+console.table(produtos)
 
+let produto1maisCaroqueproduto2 = produtoUm.valor1 > produtoDois.valor2;
+let produto1MesmoValorproduto2 = produtoUm.valor1 === produtoDois.valor2;
+let produtoMaisbarato = produtos > 50
+let produtoMaisCaro = produtos < 200
 
+console.log(`O primeiro produto e mais caro que o segundo produto: ${produto1maisCaroqueproduto2}
+    Ambos tem o mesmo preco: ${produto1MesmoValorproduto2}
+    O produto mais barato custa menos de R$50 reais: ${produtoMaisbarato}
+    O produto mais caro custa mais de R$200 reais: ${produtoMaisCaro}.`);
+*/
 console.log("_______________________________");
 
 
@@ -109,6 +131,41 @@ console.log("_______________________________");
 //      (use Math.max() e Math.min() para encontrar maior e menor)
 
 // → Seu código aqui:
+/*
+const alunoUm = {
+    nome1: lerTeclado.question('Diga um nome: '),
+    notas1: lerTeclado.questionFloat('Diga as notasdessa pessoa: ')
+};
+const alunoDois = {
+    nome2: lerTeclado.question('Diga um nome: '),
+    notas2: lerTeclado.questionFloat('Diga as notas desse aluno: ')
+};
+const alunoTres = {
+    nome3: lerTeclado.question('Diga um nome: '),
+    notas3: lerTeclado.questionFloat('Diga as notas desse aluno: ')
+};
+const alunoQuatro = {
+    nome4: lerTeclado.question('Diga um nome: '),
+    notas4: lerTeclado.questionFloat('Diga as notas desse aluno: ')
+};
+let notas = [alunoUm.notas1, alunoDois.notas2, alunoTres.notas3, alunoQuatro.notas4]
+let media = notas / 4
+
+let boletim = [
+        {nome: alunoUm.nome1, notas: alunoUm.notas1, media: media},
+        {nome: alunoDois.nome2, notas: alunoDois.notas2, media: media},
+        {nome: alunoTres.nome3, notas: alunoTres.notas3, media: media},
+        {nome: alunoQuatro.nome4, notas: alunoQuatro.notas4, media: media},
+];
+
+console.table(boletim);
+
+if(media <= 6){
+    console.log('Aprovado!');
+} else if(media >= 4 && media < 6){
+    console.log('Precisa de recuperacao.');   
+}
+*/
 
 
 console.log("_______________________________");
@@ -129,6 +186,69 @@ console.log("_______________________________");
 // e) Use template literal identificando cada produto pelo nome.
 
 // → Seu código aqui:
+
+const dadosUm = {
+    nome1: lerTeclado.question('Diga um produto: '),
+    preco1: lerTeclado.questionInt('Diga a preco desse produto: '),
+    qtdEstoque: lerTeclado.questionInt('Ha quantas unidades em estoque: ')
+};
+const dadosDois = {
+    nome2: lerTeclado.question('Diga um produto: '),
+    preco2: lerTeclado.questionInt('Diga a preco desse produto: '),
+    qtdEstoque2: lerTeclado.questionInt('Ha quantas unidades em estoque: ')
+};
+const dadosTres = {
+    nome3: lerTeclado.question('Diga um produto: '),
+    preco3: lerTeclado.questionInt('Diga a preco desse produto: '),
+    qtdEstoque3: lerTeclado.questionInt('Ha quantas unidades em estoque: ')
+};
+
+let estoque = [dadosUm, dadosDois, dadosTres];
+console.table(estoque);
+
+let ProdutoDisponivel = dadosUm.qtdEstoque > 0;
+let ProdutoDisponivel2 = dadosDois.qtdEstoque2 > 0;
+let ProdutoDisponivel3 = dadosTres.qtdEstoque3 > 0;
+
+let ProdutoAbaixoEst = dadosUm.qtdEstoque <= 5;
+let ProdutoAbaixoEst2 = dadosDois.qtdEstoque2 <= 5;
+let ProdutoAbaixoEst3 = dadosTres.qtdEstoque3 <= 5;
+
+let ProdutoCaro = dadosUm.preco1 > 100;
+let ProdutoCaro2 = dadosDois.preco2 > 100;
+let ProdutoCaro3 = dadosTres.preco3 > 100;
+
+let ProdutoPrecoAbaixo = dadosUm.preco1 < 0;
+let ProdutoPrecoAbaixo2 = dadosDois.preco2 < 0;
+let ProdutoPrecoAbaixo3 = dadosTres.preco3 < 0;
+
+
+console.log(`Primeiro produto:
+Nome: ${dadosUm.nome1}
+Preco: ${dadosUm.preco1}
+Estoque: ${dadosUm.qtdEstoque}
+Segundo produto:
+Nome: ${dadosDois.nome2}
+Preco: ${dadosDois.preco2}
+Estoque: ${dadosDois.qtdEstoque2}
+Terceiro produto:
+Nome: ${dadosTres.nome3}
+Preco: ${dadosTres.preco3}
+Estoque: ${dadosTres.qtdEstoque3}`);
+console.log(`O produto tem estoque disponivel: ${ProdutoDisponivel}
+    O produto tem estoque disponível: ${ProdutoDisponivel2}
+    O produto tem estoque disponível: ${ProdutoDisponivel3}.`);
+console.log(`O produto está com estoque baixo: ${ProdutoAbaixoEst}
+    O produto está com estoque baixo: ${ProdutoAbaixoEst2}
+    O produto está com estoque baixo: ${ProdutoAbaixoEst3}`);
+console.log(`O produto é considerado caro: ${ProdutoCaro}
+    O produto é considerado caro: ${ProdutoCaro2}
+    O produto é considerado caro: ${ProdutoCaro3}`);
+console.log(`O produto tem preço abaixo de 50: ${ProdutoPrecoAbaixo}
+    O produto tem preço abaixo de 50: ${ProdutoPrecoAbaixo2}
+    O produto tem preço abaixo de 50: ${ProdutoPrecoAbaixo3}`);
+
+
 
 
 console.log("_______________________________");
