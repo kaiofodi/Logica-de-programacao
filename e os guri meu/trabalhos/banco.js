dc665ed434dacf13017e1c867e92832970a9b65e
KAIO ROBERTO FODI <kaio_fodi@estudante.sesisenai.org.br>
2026-06-17 19:53:12 -0300

    nova-pasta-pra-trabalhos

---
+++ e os guri meu/trabalhos/banco.js
@@ -0,0 +1,20 @@
+//=================================================================
+//                    BANCO MONETARIO - AVALIATIVO
+//=================================================================
+// Desenvolver um sistema de banco monetario
+
+
+//function Menu()
+//function Nofificacoes()
+//function Acesso()
+//function Deposito()
+//function Saque()
+//function Transferencia PIX/TED/DOC()
+//function Emprestimo()
+//function Investimento()
+//function Extrato()
+//function chat()
+//function cashback()
+//function recargaCelular()
+
+//let objeto(conta, limites, tipo, infos pessoais,)
